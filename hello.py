6789417a679452a42f9736d6261bb634c0b09cf2
CryptoAngel1024 <cryptoangel1024@gmail.com>
2022-07-22 16:43:49 +0300

    mongoDB_initial

--- hello.py
+++ hello.py
@@ -1,9 +1,6 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World'
-
-if __name__ == '__main__':
-    app.run(debug = True)
+app.config["MONGO_URI"] = "mongodb+srv://dev:opensea2022.@orca.8feyo.mongodb.net/test"
+mongo = PyMongo(app)
