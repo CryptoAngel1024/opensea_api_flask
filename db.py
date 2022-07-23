6c4078f68ffb983df981f7a9ea83ad83058a5662
CryptoAngel1024 <cryptoangel1024@gmail.com>
2022-07-23 13:58:51 +0300

    server_update

--- db.py
+++ db.py
@@ -1,9 +1,8 @@
-from http import client
 from flask import Flask
 from flask_pymongo import pymongo
+from app import app
 
 CONNECTION_STRING="mongodb+srv://dev:opensea2022.@orca.8feyo.mongodb.net/test"
 client = pymongo.MongoClient(CONNECTION_STRING)
-print(client.list_database_names())
-db = client.get_database('opensea')
-trades = pymongo.collection.Collection(db, 'trades')
+db = client.opensea
+trades = db.trades
